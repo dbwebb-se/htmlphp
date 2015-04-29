fcc5f7ce98a2f30f9b5fedb692be325b0e3d7ce4
Mikael Roos <mikael.t.h.roos@gmail.com>
2015-04-29 15:37:17 +0200

    adding me-mos

--- TODO.md
+++ TODO.md
@@ -43,3 +43,5 @@ Gå igenom installation av XAMPP och var filerna ligger
 Filezilla och dbwebb publish för uppladdning av filer.
 
 SQLite 20
+
+Felmeddelande i PHP-kod.
---
+++ example/me/me-mos.php
@@ -0,0 +1,26 @@
+<?php include("incl/header.php"); ?>
+
+    <h1>Om Mig Själv</h1>
+    
+    <p>Detta är min me-sida för kursen htmlphp, så här kan det se ut. Lägg gärna tid och kraft på att göra din-me-sida personlig och använd det du lär dig i kursen för att underhålla denna webbplats. Kom ihåg att detta är det resultat du kommer att lämna in för <a href="http://dbwebb.se/kurser/bedomning-och-betygsattning">bedömning och betygsättning</a> när kursen är klar.</p>
+
+    <p>Så, en presentation en bra början. Skriv några ord om dig själv. Jag börjar.</p>
+
+    <figure class="right">
+        <img src="img/me.jpg" class="me" alt="Bild på Mikael Roos">
+        <figcaption>Mikael Roos när han går ut högstadiet.</figcaption>
+    </figure>
+
+    <p>Mitt namn är Mikael Roos. Född och uppvuxen i Bankeryd, Småland, strax utanför Jönköping, i ett villaområde som byggdes upp samtidigt som vi flyttade in där. Där växte jag upp, gick i skolan bara hundra meter från bostaden, spelade landhockey på somrarna, utmanades granngatorna på prestigefyllda matcher, spelade bandy på vintern i hårda derbyn med grannorterna.</p>
+
+    <p>Jag växte upp och på ett bananskal halkade jag in på Högskolan i Karlskrona/Ronneby, som den hette i början. Detta var i slutet av 80-talet. Tanken var att jag skulle bli kock, eller egentligen, jag visste inte riktigt vad jag ville bli men efter en vintersäsong i Stöten i Sälen och en sommarsäsong i Borgholm på Öland så tänkte jag att restaurang-branschen verkar trevlig och kock låter bra. Men mina planer på en AMF-kurs till kock grusades då en gammal gymnasielärare hjälpte mig in på en högskoleutbildning i Svängsta - *Programvaruteknik* - två veckor innan den skulle starta.</p>
+
+    <p>Programmering och datorer har alltid intresserat mig och min första datorn fick jag som 13 - 14-åring. Det var en <a href="http://en.wikipedia.org/wiki/SV-328">Spectravideo 328</a>. Högskolan har också intresserat mig men jag trodde mer att jag skulle gå nån matematikerlinjen i Växjö, om jag bara hade kommit in. Men nu var jag inne och ämnet var Programvaruteknik. Så kan det gå.</p>
+
+    <p>Efter högskolan blev jag kvar i Ronneby, jobb, fru, familj, villa, volvo, sommarstuga och så vidare och här är vi nu. Sedan efter högskolan så har jag sysslat med programvaruteknik i olika varianter. Jag har jobbat på högskolan, i storföretag i näringslivet, haft eget företag i olika varianter och nu är jag då tillbaka i rollen som undervisare på högskolan igen. Webben har alltid intresserat mig, ända sedan skoltiden. Jag minns när jag satt på mitt första jobb på högskolan, jag programmerade ett schemabokningssystem och ville visa upp de aktuella bokningarna över <a href="http://en.wikipedia.org/wiki/Gopher_(protocol)">Gopher</a> när en gammal klasskamrat kom in och berättade om en ny cool webbläsare som hette <a href="http://en.wikipedia.org/wiki/NCSA_Mosaic">NCSA Mosaic</a> och på den vägen är det.</p>
+
+    <p>Om jag skall nämna någon hobby, förutom webbprogrammering, så får det bli att bära sten på sommarstugetomten, och det finns sten så det räcker ett par livstider. Till och från får jag för mig att börja på lite hobbies, ett år satsade jag på pokerspel, ett annat år var det geocaching och sen var det turfing. Just nu letar jag inte efter fler sådana sysslor, tiden rinner i väg i alla fall. Men dyker det upp nåt kul så kan jag alltid hoppa på det.</p>
+
+    <p>Vill du veta mer så hittar du mig på de vanliga platserna som <a href="http://mikaelroos.se/googleplus">Google+</a>, <a href="http://mikaelroos.se/linkedin">LinkedIn</a>, <a href="http://mikaelroos.se/instagram">Instagram</a>, <a href="http://mikaelroos.se/facebook">Facebook</a>, <a href="http://mikaelroos.se/youtube">Youtube</a>, <a href="http://mikaelroos.se/flickr">Flickr</a>, <a href="http://mikaelroos.se/github">GitHub</a> och <a href="http://mikaelroos.se/twitter">Twitter</a>.</p>
+
+<?php include("incl/footer.php"); ?>
