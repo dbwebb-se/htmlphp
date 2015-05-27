f71ccb95f5df87ae3d460b61ffa177ab7ff883a3
Mikael Roos <mikael.t.h.roos@gmail.com>
2015-05-27 16:56:39 +0200

    adding example for stylechooser

---
+++ example/style-chooser/aside.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="?page=intro">Intro</a></li>
+        <li><a href="?page=current">Print current value of the session and stylesheet</a></li>
+        <li><a href="?page=get">Change stylesheet using $_GET</a></li>
+        <li><a href="?page=dropdown">Change stylesheet using a form dropdown</a></li>
+    </ul>
+</nav>
