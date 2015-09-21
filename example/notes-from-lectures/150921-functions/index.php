e23756ba451800d7a0b995d3923aae13036cb062
Mikael Roos <mikael.t.h.roos@gmail.com>
2015-09-21 13:05:25 +0200

    initial

---
+++ example/notes-from-lectures/150921-functions/index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<meta charset0"utf-8"/>
+<style>
+html {
+    background-color: #222;
+    color: #0f0;
+}
+body {
+    margin: 8px auto;
+    width: 800px;
+    font-size: 20px;
+}
+</style>
+
+<?php 
+echo "Hello World";
+
+/**
+ * Use some builtin functions.
+ *  http://php.net/manual/en/funcref.php
+ *
+ * Start with md5(). Check up string functions, array, math, date & time.
+ */
+ 
+/**
+ * Create my own functions
+ *  http://php.net/manual/en/language.functions.php
+ *
+ * Pure, arguments, returnvalue, typehints.
+ * Perhaps dice. Perhaps histogram.
+ * Return html as heredoc (and parse variables).
+ */
