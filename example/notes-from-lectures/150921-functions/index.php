f410f1299c5601f02fdb3978b42f6b287cb11b9c
Mikael Roos <mikael.t.h.roos@gmail.com>
2015-09-21 15:02:34 +0200

    done

--- example/notes-from-lectures/150921-functions/index.php
+++ example/notes-from-lectures/150921-functions/index.php
@@ -7,13 +7,16 @@ html {
 }
 body {
     margin: 8px auto;
-    width: 800px;
-    font-size: 20px;
+    margin-top: 20px;
+    width: 600px;
+    font-size: 28px;
 }
 </style>
 
+<pre>
+
 <?php 
-echo "Hello World";
+echo "Hello World\n\n";
 
 /**
  * Use some builtin functions.
@@ -21,12 +24,82 @@ echo "Hello World";
  *
  * Start with md5(). Check up string functions, array, math, date & time.
  */
- 
+$password = "Mumintrollet68";
+$hash = md5($password);
+
+echo "The string:\n$password\ngenerated the md5 hash of:\n$hash";
+
+
+$x = 27;
+$y = 97;
+$z = 3;
+
+$maxValue = max($x, $y, $z);
+
+echo "\n\nMax value is: $maxValue\n";
+
+$minValue = min($x, $y, $z);
+
+echo "\n\nMin value is: $minValue\n";
+
+
 /**
  * Create my own functions
  *  http://php.net/manual/en/language.functions.php
  *
  * Pure, arguments, returnvalue, typehints.
- * Perhaps dice. Perhaps histogram.
+ * Perhaps dice. Perhaps histogram. Perhaps card.
  * Return html as heredoc (and parse variables).
  */
+
+
+
+/**
+ * Throw the dice.
+ */
+function throwDice() {
+    $value = rand(1, 6);
+ 
+    return $value;
+}
+
+
+
+/**
+ * Sum the dices in the hand.
+ */
+function sumDiceHand($aHand) {
+    //global $hand;
+    
+    return array_sum($aHand);
+}
+
+
+
+$hand = [];
+for ($i = 0; $i < 5; $i++) {
+    $hand[] = throwDice();
+}
+
+//echo "\n\nDicehand contains " . count($hand) . " values like this:\n" . print_r($hand, true) . "\n";
+echo "\n\nDicehand contains "
+    . count($hand)
+    . " values like this:\n"
+    . implode(", ", $hand)
+    . "\n";
+    
+echo "\n\nSum of hand is: " . sumDiceHand($hand) . "\n";
+
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
