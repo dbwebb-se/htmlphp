11d0b1be906c6ab9f9e0110a333eaeaf9feb1df3
Mikael Roos <mikael.t.h.roos@gmail.com>
2018-06-19 09:23:02 +0200

    Move vide examples to old

--- example/video-learn-php/10x-friday/mall.php
+++
@@ -1,6 +0,0 @@
-<!doctype html>
-<meta charset="utf-8">
-<title>Kom igång med PHP</title>
-
-<h1>Kom igång med PHP</h1>
-<hr>
--- example/video-learn-php/mall.php
+++
@@ -1,9 +0,0 @@
-<!doctype html>
-<meta charset="utf-8">
-<link href="style.css" rel="stylesheet" type="text/css">
-<title>Kom igång med PHP</title>
-
-<h1>Kom igång med PHP</h1>
-<hr>
-<pre>
-$ <i>And what now?</i>
