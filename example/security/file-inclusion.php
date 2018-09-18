8de1d7f3b589c18c0041ee0f894f2a198fd44a53
Mikael Roos <mikael.t.h.roos@gmail.com>
2018-09-18 17:08:39 +0200

    Add security examples

---
+++ example/security/file-inclusion.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Example to show off how security works with file-inclusion.
+ *
+ * Access this page using this url (modify to your environment):
+ * ?page=../../composer.json
+ * ?page=../../../../etc/passwd
+ * ?page=../../../../var/log/php.log
+ */
+
+ // The demo code is commentet out by default, remove the starting comment
+ // to make it run.
+/*
+?>
+<p>Demo start: file inclusion</p>
+<?= $_GET["page"] ?>
+<hr>
+<pre>
+<?php require "{$_GET["page"]}"; ?>
+</pre>
+<hr>
+<p>Demo end.</p>
---
+++ example/security/xss.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Example to show off how XSS is done and how to protect yourself.
+ *
+ * Access this page using this url:
+ * ?page=<script>alert("You got XSS:ed. Got ya.");</script>
+ *
+ */
+
+// The demo code is commentet out by default, remove the starting comment
+// to make it run.
+
+/*
+?>
+<p>Demo start: XSS.</p>
+<?= $_GET["page"] ?>
+<hr>
+<?= htmlentities($_GET["page"]); ?>
+<p>Demo end.</p>
