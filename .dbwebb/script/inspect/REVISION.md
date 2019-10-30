186e7bde4f8ad058bce296efdc7f90a13a8aa754
Mikael Roos <mikael.t.h.roos@gmail.com>
2019-10-30 13:10:52 +0100

    Megafix. 2.

---
+++ .dbwebb/script/inspect/REVISION.md
@@ -0,0 +1,17 @@
+Revision history
+==================================
+
+
+
+v1.5.0 (2019-10-25)
+----------------------------------
+
+* Merge functions.bash into gui.bash.
+
+
+
+v1.4.4 (2019-10-22)
+----------------------------------
+
+* Lastest version before merging gui.bash to one. Using htmlphp gui.bash as base.
+* 
--- .dbwebb/script/inspect/gui.bash
+++ .dbwebb/script/inspect/gui.bash
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
+#
+#
 
-VERSION="v1.4.4 (2019-10-22)"
+# ---------------------------- Application wide settings ------------------
 
-# Include ./functions.bash
+VERSION="v1.5.0 (2019-10-25)"
+
+
+# Get current dir 
+# @TODO Make this more flexible to be run at any level and find the
+#       course repo automatically.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-# shellcheck source=.
-source "$DIR/../functions.bash"
 
 # Source the config file if it exists
 DBWEBB_GUI_CONFIG_FILE="$HOME/.dbwebb/gui_config.bash"
@@ -31,6 +36,7 @@ source "$DIR/../../../.dbwebb.course"
 COURSE="$DBW_COURSE"
 LOGFILE="log-gui-main.ansi"
 LOGFILE_INSPECT="log-gui-inspect.ansi"
+LOGFILE_TEXT="log-gui-text.ansi"
 BACKTITLE="dbwebb/$COURSE"
 TITLE="Work with kmoms"
 REDOVISA_HTTP_PREFIX="http://www.student.bth.se"
@@ -68,6 +74,167 @@ dockerContainer="mysql"
 
 # Remember last menu choice (or set defaults)
 mainMenuSelected=1
+
+
+# ---------------------------- General functions -------------------------
+# # Include ./functions.bash
+# DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# # shellcheck source=.
+# source "$DIR/../functions.bash"
+
+#
+# Press enter to continue
+#
+pressEnterToContinue()
+{
+    local void
+
+    if [[ ! $YES ]]; then
+        printf "\nPress enter to continue..."
+        read void
+    fi
+}
+
+
+
+#
+# Read input from user supporting a default value for reponse.
+#
+# @arg1 string the message to display.
+# @arg2 string the default value.
+#
+input()
+{
+    read -r -p "$1 [$2]: "
+    echo "${REPLY:-$2}"
+}
+
+
+
+#
+# Read input from user replying with yY or nN.
+#
+# @arg1 string the message to display.
+# @arg2 string the default value, y or n.
+#
+yesNo()
+{
+    
+    read -r -p "$1 (y/n) [$2]: "
+    echo "${REPLY:-$2}"
+}
+
+
+
+#
+# Display information message and wait for user input to continue, exit if
+# user presses 'q'.
+#
+# @arg1 string the message to display.
+#
+info()
+{
+    echo -e "\n$1"
+    read -r -p ""
+    case "$REPLY" in
+        q)
+            exit 1
+            ;;
+    esac
+}
+
+
+
+#
+# Fail, die and present error message.
+#
+# @arg1 string the message to display.
+# @arg2 string exit status (default -1).
+#
+die()
+{
+    local message="$1"
+    local status="${2:-1}"
+
+    printf "$MSG_FAILED $message\n" >&2
+    exit $status
+}
+
+
+
+#
+# Set correct settings of the remote student files, populary called "potatoe".
+#
+# @arg1 string the acronym.
+#
+potatoe()
+{
+    local acronym
+    local course="$COURSE"
+
+    if [[ $2 = "false" ]]; then
+        course=
+    fi
+
+    acronym=$( input "Akronym att uppdatera rättigheter för?" "$1" )
+    dbwebb run "sudo /usr/local/sbin/setpre-dbwebb-kurser.bash $acronym $course"
+    #dbwebb run "sudo /usr/local/sbin/setpre-dbwebb-kurser.bash $acronym"
+}
+
+
+
+#
+# Create a config file that are sourced each time the application starts.
+#
+createConfigFile()
+{
+    local source="$DIR/gui_config.bash"
+    local reply=
+
+    if [[ -f $DBWEBB_GUI_CONFIG_FILE ]]; then
+        reply=$( yesNo "Du har redan en konfigurationsfil, vill du skriva över den?" "n" )
+        case "$reply" in
+            [yY]) ;;
+            *) printf "Ignoring..." && return ;;
+        esac
+    fi
+
+    install -d "$( dirname $DBWEBB_GUI_CONFIG_FILE )"
+    cp "$source" "$DBWEBB_GUI_CONFIG_FILE" \
+        && printf "Konfigurationsfilen är nu skapad, redigera den i en texteditor.\n" \
+        && ls -l "$DBWEBB_GUI_CONFIG_FILE"
+
+    # shellcheck source=$HOME/.dbwebb.gui_config.bash
+    [[ -f $DBWEBB_GUI_CONFIG_FILE ]] && source "$DBWEBB_GUI_CONFIG_FILE"
+}
+
+
+
+#
+# Goto directory and check its status on .git
+# @arg1 string the path to move to.
+#
+check_dir_for_git()
+{
+    pushd $1
+    if [ ! -d .git ]; then
+        echo "No git-repo on highest level: $1"
+        ls -l
+        return
+    fi
+
+    # Show details
+    git log --pretty=format:"%h %ad | %s%d [%an]" --graph --date=short
+    echo "$1"
+    git remote -v
+    git tag
+
+    # Checkout version and set it up
+    TAG=$( input "Checkout tag" "" )
+    git checkout $TAG
+    npm install
+    popd
+}
 
 
 
@@ -502,17 +669,17 @@ feedback()
     local kmom="$1"
     local output
 
-    header "Feedback" | tee -a "$LOGFILE"
+    header "Feedback" | tee -a "$LOGFILE_TEXT"
 
     output=$( eval echo "\"$( cat "$DIR/text/$kmom.txt" )"\" )
     #output=$(< "$DIR/text/$kmom.txt" )
-    printf "\n%s\n\n" "$output" | tee -a "$LOGFILE"
+    printf "\n%s\n\n" "$output" | tee -a "$LOGFILE_TEXT"
     printf "%s" "$output" | eval $TO_CLIPBOARD
 
     if [[ -f "$DIR/text/${kmom}_extra.txt" ]]; then
         #output=$( eval echo "\"$( cat "$DIR/text/${kmom}_extra.txt" )"\" )
         output=$(< "$DIR/text/${kmom}_extra.txt" )
-        printf "\n\033[32;01m---> Vanliga feedbacksvar\033[0m\n\n%s\n\n" "$output" | tee -a "$LOGFILE"
+        printf "\n\033[32;01m---> Vanliga feedbacksvar\033[0m\n\n%s\n\n" "$output" | tee -a "$LOGFILE_TEXT"
     fi
 }
 
