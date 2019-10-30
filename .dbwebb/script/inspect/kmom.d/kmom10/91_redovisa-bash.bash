6cebe6a7545f850f59bed59fa7141a8b29c0289f
Mikael Roos <mikael.t.h.roos@gmail.com>
2019-10-30 15:28:23 +0100

    Adding setup for kmom10 to gui.bash

--- .dbwebb/script/inspect/kmom.d/kmom10/91_redovisa-bash.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-cd me/redovisa || exit
-echo "Do manual stuff, if needed (write exit to exit)?"
-ls
-bash
--- .dbwebb/script/inspect/kmom.d/kmom10/pre.bash
+++ .dbwebb/script/inspect/kmom.d/kmom10/pre.bash
@@ -2,7 +2,16 @@
 
 printf ">>> -------------- Pre inspect -------------------------\n"
 
-# Open me/kmom10/proj
-printf "$REDOVISA_HTTP_PREFIX/~$acronym/dbwebb-kurser/$COURSE/me/kmom10/proj/htdocs\n" 2>&1
+# # Open localhost:1337 in browser
+# printf "Open localhost:1337/eshop/index in browser\n"
+# eval "$BROWSER" "http://127.0.0.1:1337/eshop/index" &
 
-eval "$BROWSER" "$REDOVISA_HTTP_PREFIX/~$acronym/dbwebb-kurser/$COURSE/me/kmom10/proj/htdocs" &
+# 
+printf "$REDOVISA_HTTP_PREFIX/~$acronym/dbwebb-kurser/$COURSE/me/kmom06/me6\n" 2>&1
+eval "$BROWSER" "$REDOVISA_HTTP_PREFIX/~$acronym/dbwebb-kurser/$COURSE/me/kmom06/me6" &
+
+# 
+printf "$REDOVISA_HTTP_PREFIX/~$acronym/dbwebb-kurser/$COURSE/me/kmom10\n" 2>&1
+eval "$BROWSER" "$REDOVISA_HTTP_PREFIX/~$acronym/dbwebb-kurser/$COURSE/me/kmom10" &
+
+echo
